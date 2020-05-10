04e712b11cd0b366fb2d85d849687a6de68da265
apbatth062 <batthamrit04@gmail.com>
2020-05-10 17:24:38 +0530

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Card, CardImg, CardBody,CardText, CardTitle} from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-    
+
+
     function RenderDish({dish}) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -17,7 +20,7 @@ import { Card, CardImg, CardBody,CardText, CardTitle} from "reactstrap";
         }
 
     function RenderComments({comments}){
-      
+        // console.log(comments)
         if (comments != null) {
 
             let list = comments.map((comments)=>{
@@ -52,15 +55,26 @@ import { Card, CardImg, CardBody,CardText, CardTitle} from "reactstrap";
 
     const DishDetail = (props) => {
 
-       
         if (props.dish != null) {
             return(
                 <div className="container">
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                            <RenderDish dish={props.dish} />
+                            <RenderComments comments={props.comments} />
+                    </div>
                 </div>
-            </div>
             )
         }else{
             return(
